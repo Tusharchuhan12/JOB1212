98db251a35c369deabb37438f40d49f334d92f95
tushar chauhan <tusharchauhan120104@gmail.com>
2025-07-13 19:22:15 +0530

    save

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ app.use('/api/jobs', jobRoutes)
 app.use('/api/users', userRoutes)
 
 // Port
-const PORT = process.env.PORT || 5002
+const PORT = process.env.PORT || 50000
 
 Sentry.setupExpressErrorHandler(app);
 
